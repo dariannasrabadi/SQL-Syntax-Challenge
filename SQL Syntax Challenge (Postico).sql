7a69955a936f356d38dc8f87ed20a22949c79288
Darian N <31804469+dariannasrabadi@users.noreply.github.com>
2018-01-09 13:55:31 -0600

    Answer questions 1-3

--- SQL Syntax Challenge (Postico).sql
+++ SQL Syntax Challenge (Postico).sql
@@ -17,3 +17,39 @@ INSERT INTO accounts (username, city, transactions_completed, transactions_attem
 ('ora', 'phoenix', 88, 90, 3.33),
 ('grace', 'miami', 7, 9100, 34.78),
 ('hope', 'phoenix', 4, 10, 50.17);
+
+-- 1	Get all users from Chicago.
+
+SELECT * FROM accounts
+WHERE city = 'chicago';
+
+-- 2	Get all users with usernames that contain the letter a.
+
+SELECT * FROM accounts
+WHERE username ILIKE '%a%';
+
+-- 3	The bank is giving a new customer bonus! Update all records with an account balance of 0.00 and a transactions_attempted of 0. Give them a new account balance of 10.00.
+
+SELECT * FROM accounts -- SELECT first to check if data I want to change is right
+WHERE account_balance = 0 AND transactions_attempted = 0;
+
+UPDATE accounts -- Updating above selection to reflect change.
+SET account_balance = 10.00 
+WHERE account_balance = 0 AND transactions_attempted = 0;
+
+-- 4	Select all users that have attempted 9 or more transactions.
+
+
+-- 5	Get the username and account balance of the 3 users with the highest balances, sort highest to lowest balance. NOTE: Research LIMIT
+
+
+-- 6	Get the username and account balance of the 3 users with the lowest balances, sort lowest to highest balance.
+
+
+-- 7	Get all users with account balances that are more than $100.
+
+
+-- 8	Add a new record.
+
+
+-- 9	The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: Delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
