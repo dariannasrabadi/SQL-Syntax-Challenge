8af0617d07dddf3c10dd6163b7e6dd829940a520
Darian N <31804469+dariannasrabadi@users.noreply.github.com>
2018-01-09 14:09:53 -0600

    Answer Questions 6 - 9

--- SQL Syntax Challenge (Postico).sql
+++ SQL Syntax Challenge (Postico).sql
@@ -54,11 +54,15 @@ ORDER BY account_balance LIMIT 3;
 
 -- 7	Get all users with account balances that are more than $100.
 
-
+SELECT * FROM accounts
+WHERE account_balance > 100;
 
 -- 8	Add a new record.
 
-
+INSERT INTO accounts (username, city, transactions_completed, transactions_attempted, account_balance)
+VALUES ('darian', 'minneapolis', 1, 1, 999999.99);
 
 -- 9	The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: Delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
 
+SELECT * FROM accounts
+WHERE transactions_completed < 5 AND city = 'miami' OR transactions_completed < 5 AND city = 'phoenix';
