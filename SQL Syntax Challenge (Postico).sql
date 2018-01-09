31d25c8adff72e6f1647483b3469cd7a00d56e66
Darian N <31804469+dariannasrabadi@users.noreply.github.com>
2018-01-09 14:03:26 -0600

    Answer Questions 4-6

--- SQL Syntax Challenge (Postico).sql
+++ SQL Syntax Challenge (Postico).sql
@@ -39,17 +39,26 @@ WHERE account_balance = 0 AND transactions_attempted = 0;
 
 -- 4	Select all users that have attempted 9 or more transactions.
 
+SELECT * FROM accounts
+WHERE transactions_attempted > 8;
 
 -- 5	Get the username and account balance of the 3 users with the highest balances, sort highest to lowest balance. NOTE: Research LIMIT
 
+SELECT username, account_balance FROM accounts
+ORDER BY account_balance DESC LIMIT 3;
 
 -- 6	Get the username and account balance of the 3 users with the lowest balances, sort lowest to highest balance.
 
+SELECT username, account_balance FROM accounts
+ORDER BY account_balance LIMIT 3;
 
 -- 7	Get all users with account balances that are more than $100.
+
 
 
 -- 8	Add a new record.
 
 
+
 -- 9	The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: Delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
+
